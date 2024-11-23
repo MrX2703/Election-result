548ad6429be66d69703cc5ad784b3b9dc2fac155
Nilesh <99276714+MrX2703@users.noreply.github.com>
2024-11-23 13:59:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const path = require("path");
+import express from "express";
+import fetch from "node-fetch";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
